476e057915bee44d40b29a3d28cb54ac3a13477a
katou-risa <katou.risa@comnic.co.jp>
2020-06-16 09:39:19 +0900

    commit (commentout)

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 class Header extends Component {
     constructor(props) {
         super(props);
@@ -7,11 +8,14 @@ class Header extends Component {
     }
     render() {
         return (
-
-            <h1>家計簿アプリ</h1>
-
+            <div>
+                <h1>家計簿アプリ</h1>
+            </div>
         );
     }
 }
 
 export default Header;
+
+// const className2 = { color: "bg-info" };
+// * <h1 style={className2} >家計簿アプリ</h1> */ }
